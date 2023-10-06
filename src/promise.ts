30df69bb3af6b9b49b66873871d2e0b8bb638aed
Nikita Khodakovsky <bednayadusha@gmail.com>
2023-10-06 15:05:35 +0300

    Create basic Promise implementation

---
+++ src/promise.ts
@@ -0,0 +1,134 @@
+const Fallback = {
+    onFulfilled: (v: unknown) => {
+        return v
+    },
+    onRejected: (r: unknown) => {
+        throw r
+    }
+}
+
+enum Status {
+    PENDING,
+    FULFILLED,
+    REJECTED
+}
+
+export type Resolve = <T>(value: T | PromiseLike<T>) => any
+export type Reject = (reason: any) => any
+export type Executor = (resolve: Resolve, reject: Reject) => any
+
+export type FulfilledHandler<T, R> = (value: T) => R | PromiseLike<R>
+export type RejectedHandler<R> = (reason: any) => R | PromiseLike<R>
+export type FinallyHandler = () => any
+
+export class PromiseImplementation<T> implements PromiseLike<T> {
+    protected deferred: [PromiseImplementation<unknown>, unknown, unknown][] = []
+    protected status = Status.PENDING
+    protected resolved = false
+    protected result: any
+
+    constructor(executor?: Executor) {
+        let isCalled = false
+
+        const resolve: Resolve = (value) => {
+            if (isCalled) return
+            isCalled = true
+            this._resolve(value)
+        }
+
+        const reject: Reject = (reason) => {
+            if (isCalled) return
+            isCalled = true
+            this._reject(reason)
+        }
+
+        try {
+            typeof executor === 'function' && executor(resolve.bind(this), reject.bind(this))
+        } catch (e) {
+            reject(e)
+        }
+    }
+
+    /* Implementation of Promise Resolution Procedure according to the PromiseA+ specification. */
+    protected _resolve(x: any) {
+        const promise = this
+
+        if (promise === x) {
+            promise._reject(new TypeError('promise === x'))
+        } else if (x !== null && (typeof x === 'object' || typeof x === 'function')) {
+            let isHandlerCalled = false
+
+            try {
+                const then = x.then
+
+                if (typeof then === 'function') {
+                    then.call(
+                        x,
+                        (y: unknown) => {
+                            if (isHandlerCalled) return
+                            isHandlerCalled = true
+                            promise._resolve(y)
+                        },
+                        (r: unknown) => {
+                            if (isHandlerCalled) return
+                            isHandlerCalled = true
+                            promise._reject(r)
+                        }
+                    )
+                } else {
+                    promise._fulfill(x)
+                }
+            } catch (e) {
+                !isHandlerCalled && promise._reject(e)
+            }
+        } else {
+            promise._fulfill(x)
+        }
+    }
+
+    protected propagate() {
+        if (this.status === Status.PENDING) return
+
+        setTimeout(() => {
+            for (const [promise, onFulfilled, onRejected] of this.deferred) {
+                const callback = this.status === Status.FULFILLED ? onFulfilled : onRejected
+                const fallback = this.status === Status.FULFILLED ? Fallback.onFulfilled : Fallback.onRejected
+                const handler = typeof callback === 'function' ? callback : fallback
+
+                try {
+                    promise._resolve(handler(this.result))
+                } catch (e) {
+                    promise._reject(e)
+                }
+            }
+
+            this.deferred = []
+        }, 0)
+    }
+
+    protected _fulfill(value: T) {
+        this.settle(Status.FULFILLED, value)
+    }
+
+    protected _reject(reason: any) {
+        this.settle(Status.REJECTED, reason)
+    }
+
+    protected settle(status: Status, result: unknown) {
+        if (this.status === Status.PENDING) {
+            this.status = status
+            this.result = result
+            this.propagate()
+        }
+    }
+
+    public then<R1 = T, R2 = never>(onFulfilled?: FulfilledHandler<T, R1> | null, onRejected?: RejectedHandler<R2> | null): PromiseLike<R1 | R2> {
+        const promise = new PromiseImplementation<R1 | R2>()
+
+        this.deferred.push([promise, onFulfilled, onRejected])
+
+        this.propagate()
+
+        return promise
+    }
+}
